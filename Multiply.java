0290a421070708452b57f1bb3d6b48bc90d4f87c
Nathan377 <kawulan@uwplatt.edu>
2020-10-04 11:21:58 -0500

    Create Multiply.java

---
+++ Multiply.java
@@ -0,0 +1,13 @@
+public class Multiply{
+    int num1;
+    int num2;
+
+    Multiply(int num1, int num2){
+        this.num1 = num1;
+        this.num2 = num2;
+    }
+
+    public int doMultiplication(){
+        return this.num1 * this.num2;
+    }
+}
